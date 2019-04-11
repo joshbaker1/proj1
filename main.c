088fc4518fcf72d65d9b70790e3c8b24a2014a6b
Joshua Baker <jbaker7@uon.edu.au>
2019-04-11 05:58:51 +0000

    changd input method and set up menu system

--- main.c
+++ main.c
@@ -4,11 +4,64 @@
 
 int main() {
     
-//INITIALISATION and DECLARATION    
+    /*
+    FILE *rotCyph;
+    FILE *subCyph;
+    FILE *encryptOutput;
+    
+    rotCyph = fopen("rotCyph.text", "r");
+    subCyph = fopen("subCyph.text", " ");   */
+    
+    //int rotationCypher(int key, /*message*/);
+    
+    //int rotationEncryption(int key, /*message*/);
+    
+    //int substitutionCypher(, /*message*/);
+    
+    //int substitutionEncryption( /*message*/);
+
+    
     //char letters[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};  
-                                                              //initialise array of letters
-    //int rotationkey = 1;                                      // set the "key" to = 1
-    char message[100];                                        // the message that will be deciphered
+    
+    //int key;
+    int programSelection;
+    char message[] = {"Brannen has an above average sized "};
+    
+    printf("Press 1 for rotation cypher\nPress 2 for rotation encryption\nPress 3 for substitution cypher\nPress 4 for substitution encryption\n ");
+    scanf("%d", &programSelection);
+    
+    switch(programSelection) {
+        
+        case 1:
+            printf("You selected rotation cypher\n" );
+            printf("%s\n", message);
+            return 1;
+            break;
+             
+        case 2:
+            printf("You selected rotation encryption\n" );
+            //do function
+            break;
+             
+        case 3:
+            printf("you selected substitution cypher\n" );
+             //do function
+             break;
+         
+        case 4:
+            printf("You selected substitution encryption\n" );
+            //do function         
+            break;
+         
+        default:
+            printf("invalid number please re-run program\n" );
+    }
+    
+    
+    
+    
+    
+    /*char message[100];                                        // the message that will be deciphered
     int i;                                                    //initialise i which will be the index of the array letters
     int j = strlen(message);                                  // sets j to = the amount of characters in the array message
 
@@ -18,7 +71,7 @@ int main() {
     
     for(i = 0; i <= j; i++) {
         
-    }
+    } */
   
  
 
